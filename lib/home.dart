4cfe24fa645bea73a7c5de37f2bb05aaa20c3279
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-04-03 20:07:27 +0530

    Frontend Sprites

--- lib/home.dart
+++ lib/home.dart
@@ -1,30 +1,71 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> todoItems = [];
+  TextEditingController _textEditingController = TextEditingController();
+
+  void _addTodoItem() {
+    if (_textEditingController.text.isNotEmpty) {
+      setState(() {
+        todoItems.add(_textEditingController.text);
+        _textEditingController.clear();
+      });
+    }
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() {
+      todoItems.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('To-Do App'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to the Home Screen!',
-              style: TextStyle(fontSize: 24),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _textEditingController,
+                    decoration: InputDecoration(
+                      hintText: 'Enter a task',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: _addTodoItem,
+                ),
+              ],
             ),
-            // Add your home screen content here
-            ElevatedButton(
-              onPressed: () {
-                // Replace with log out logic
-                Navigator.pop(context);
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: todoItems.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(todoItems[index]),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => _removeTodoItem(index),
+                  ),
+                );
               },
-              child: Text('Log Out'),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
